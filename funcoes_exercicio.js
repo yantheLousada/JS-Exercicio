91ee7363c55c49ffd23432fd4b79aca45a91e661
yantheLousada <yanthe96@hotmail.com>
2023-03-16 09:03:57 -0300

    funcoes commit

---
+++ funcoes_exercicio.js
@@ -0,0 +1,28 @@
+// Crie uma função para verificar se um valor é Truthy
+function verifica(param) {
+  return !!param;
+}
+console.log(verifica(10));
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetro(lado) {
+  return 4 * lado;
+}
+console.log(perimetro(5));
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
+console.log(nomeCompleto('yan', 'the'));
+// Crie uma função que verifica se um número é par
+function par(num) {
+  var modulo = num % 2;
+  return modulo === 0 ? true : false;
+}
+console.log(par(10));
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+function typeoff(param) {
+  return typeof param;
+}
---
+++ rascunho.js
@@ -0,0 +1,15 @@
+// Declare uma variável contendo uma string
+var string = 'frase';
+// Declare uma variável contendo um número dentro de uma string
+var stringNumero = '8';
+// Declare uma variável com a sua idade
+var idade = 26;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'yan';
+var sobrenome = 'the';
+console.log(`${nome} ${sobrenome}`);
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
