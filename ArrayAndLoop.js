f52edfb5733ef0dde4f7e029e6f3bc67e46c7153
yantheLousada <yanthe96@hotmail.com>
2023-03-21 10:08:52 -0300

    Ternario commit

--- ArrayAndLoop.js
+++ ArrayAndLoop.js
@@ -4,14 +4,15 @@ var CopasWon = [1959, 1962, 1970, 1994, 2002];
 console.log(CopasWon);
 // Interaja com a array utilizando um loop, para mostrar
 // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
-for (var item = 0; item <= CopasWon.length; item++) {
-  console.log(CopasWon[item]);
+for (var item = 0; item < CopasWon.length; item++) {
+  console.log(`O Brasil ganhou a copa de ` + CopasWon[item]);
 }
 
 // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
 var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'];
-for (var i = 0; i <= frutas[2]; i++) {
+for (var i = 0; i <= frutas.length; i++) {
   console.log(frutas[i]);
+  if (frutas[i] === 'Pera') {
+    break;
+  }
 }
-// Coloque a última fruta da array acima em uma variável,
-// sem remover a mesma da array.
