e0bfc8e4fc794dc101acdc759c52ff13e591f159
yantheLousada <yanthe96@hotmail.com>
2023-03-25 09:02:06 -0300

    DOM commit

---
+++ DOM.js
@@ -0,0 +1,12 @@
+// Retorne o url da página atual utilizando o objeto window
+const hrefPag = window.location.href;
+console.log(hrefPag);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo');
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+// Retorne a largura da janela
+const largura = window.innerWidth;
+console.log(largura);
--- Escopo.js
+++ Escopo.js
@@ -20,9 +20,9 @@ somarDois(4);
 dividirDois(6);
 //
 // O que fazer para total retornar 500?
-var numero = 50;
+const numero = 50;
 
-for (var numero = 0; numero < 10; numero++) {
+for (let numero = 0; numero < 10; numero++) {
   console.log(numero);
 }
 
