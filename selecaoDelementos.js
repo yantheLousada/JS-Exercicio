5082aebd6bd00c9d5071214c679813d3688d2e8e
yantheLousada <yanthe96@hotmail.com>
2023-03-27 21:23:04 -0300

    selecao d elementos commit

---
+++ selecaoDelementos.js
@@ -0,0 +1,8 @@
+// getElementById seleciona e retorna elementos do DOM
+
+// Seleciona pelo ID
+const animaisSection = document.getElementById('animais');
+const contatoSection = document.getElementById('contato');
+/* 
+getElementsByClassName e getElementsByTagName selecionam e retornam uma lista de elementos do DOM. A lista retornada está ao vivo, significa que se elementos forem adicionados, ela será automaticamente atualizada.
+*/
