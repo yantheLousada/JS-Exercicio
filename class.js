5a3690ace7e89fb10498a87057eb8a235fcd8fdf
yantheLousada <yanthe96@hotmail.com>
2023-04-05 09:32:16 -0300

    class commit

---
+++ class.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu');
+
+menu.className; // string
+menu.classList; // lista de classes
+menu.classList.add('ativo');
+menu.classList.add('ativo', 'mobile'); // duas classes
+menu.classList.remove('ativo');
+menu.classList.toggle('ativo'); // adiciona/remove a classe
+menu.classList.contains('ativo'); // true ou false
+menu.classList.replace('ativo', 'inativo');
