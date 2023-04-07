181597d083fa2970806e3414ad1f25d464464a37
yantheLousada <yanthe96@hotmail.com>
2023-04-07 17:57:06 -0300

    class commit

--- class.js
+++ class.js
@@ -1,10 +1,10 @@
-const menu = document.querySelector('.menu');
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a');
+itensMenu.forEach((item) => {
+  item.classList.add('ativo');
+});
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
 
-menu.className; // string
-menu.classList; // lista de classes
-menu.classList.add('ativo');
-menu.classList.add('ativo', 'mobile'); // duas classes
-menu.classList.remove('ativo');
-menu.classList.toggle('ativo'); // adiciona/remove a classe
-menu.classList.contains('ativo'); // true ou false
-menu.classList.replace('ativo', 'inativo');
+// Verifique se as imagens possuem o atributo alt
+
+// Modifique o href do link externo no menu
