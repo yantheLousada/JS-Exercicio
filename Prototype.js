4285106d8bd11d5f6cdce2bf1ad63a658433a93a
yantheLousada <yanthe96@hotmail.com>
2023-05-02 22:23:29 -0300

    prototype 2 commit

--- Prototype.js
+++ Prototype.js
@@ -1,18 +1,18 @@
 // A propriedade prototype é um objeto adicionado a uma função quando a mesma é criada.
 
-function Pessoa(nome, idade) {
-  this.nome = nome;
-  this.idade = idade;
-}
-const andre = new Pessoa('André', 28);
-
-console.log(Pessoa.prototype); // retorna o objeto
-
 // Crie uma função construtora de Pessoas
 // Deve conter nome, sobrenome e idade
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
-
+function Pessoa() {
+  this.nome = 'yan';
+  this.sobrenome = 'Thé';
+  this.idade = 26;
+}
+Pessoa.prototype.nomeCompleto = function (nome, sobrenome) {
+  return `&{nome} + &{sobrenome}`;
+};
+console.log(Pessoa.nomeCompleto('yan', 'the'));
 // Liste os métodos acessados por
 // dados criados com NodeList,
 // HTMLCollection, Document
