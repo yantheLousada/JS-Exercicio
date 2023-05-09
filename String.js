aa7b4c3e3eacee5a5559325b3ae3628aa09af402
yantheLousada <yanthe96@hotmail.com>
2023-05-09 17:27:57 -0300

    String commit

---
+++ String.js
@@ -0,0 +1,51 @@
+//É a construtora de strings, toda string possui as propriedades e métodos do prototype de String.
+
+const liquido = new String('Água');
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+/*const transacoes = [
+  {
+    descricao: 'Taxa do Pão',
+    valor: 'R$ 39',
+  },
+  {
+    descricao: 'Taxa do Mercado',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 99',
+  },
+  {
+    descricao: 'Taxa do Banco',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 49',
+  },
+];
+*/
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+console.log(transportes);
+// Substitua todos os span's por a's
+const html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
+
+// Retorne o último caracter da frase
+const frase = 'Melhor do ano!';
+
+// Retorne o total de taxas
+const transacoes = [
+  'Taxa do Banco',
+  '   TAXA DO PÃO',
+  '  taxa do mercado',
+  'depósito Bancário',
+  'TARIFA especial',
+];
