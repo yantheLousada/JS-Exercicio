81bbedd19c11585244e060febde6ad6256a15d1f
yantheLousada <yanthe96@hotmail.com>
2023-06-07 10:20:00 -0300

    Else commit

--- if.js
+++ if.js
@@ -9,3 +9,14 @@ function Check(n) {
   }
 }
 console.log(Check(n));
+
+//
+function monkeyTrouble(aSmile, bSmile) {
+  if (aSmile === true && bSmile === true) {
+    return true;
+  } else if (aSmile === false && bSmile === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
