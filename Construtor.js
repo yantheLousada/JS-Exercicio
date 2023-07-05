72342429d78959e8f1f9d117983355629d9b8254
yantheLousada <yanthe96@hotmail.com>
2023-07-05 09:44:54 -0300

    teste commit

--- Construtor.js
+++ Construtor.js
@@ -1,20 +1,32 @@
-const honda = new Carro();
+//const honda = new Carro();
 
 // 1 Cria um novo objeto
-honda = {};
+//honda = {};
 
 // 2 Define o protótipo
-honda.prototype = Carro.prototype;
+//honda.prototype = Carro.prototype;
 
 // 3 Aponta a variável this para o objeto
-this = honda;
+//this = honda;
 
 // 4 Executa a função, substituindo this pelo objeto
-honda.marca = 'Marca';
-honda.preco = 0;
+//honda.marca = 'Marca';
+//honda.preco = 0;
 
 // 5 Retorna o novo objeto
-return honda = {
+/*return honda = {
   marca: 'Marca',
   preco: 0,
+} */
+//
+function Carro() {
+  this.marca = 'Marca';
+  this.preco = 0;
 }
+
+const honda = new Carro();
+honda.marca = 'Honda';
+honda.preco = 4000;
+const fiat = new Carro();
+fiat.marca = 'Fiat';
+fiat.preco = 3000;
