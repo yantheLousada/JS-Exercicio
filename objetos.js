e2ee26c5152599ba43c9332f34b78c0870f5e93e
yantheLousada <yanthe96@hotmail.com>
2023-08-08 07:22:39 -0300

    tipos de obj commit

--- objetos.js
+++ objetos.js
@@ -26,3 +26,17 @@ var cachorro = {
   },
 };
 console.log(cachorro);
+/*
+Native
+Objetos nativos são aqueles definidos na especificação da linguagem e são implementados independente do host - Object
+String
+Array
+Function
+Host
+Objetos do host são aqueles implementados pelo próprio ambiente. Por exemplo no browser possuímos objetos do DOM, como DomList, HTMLCollection e outros. Em Node.js os objetos do Host são diferentes, já que não estamos em um ambiente do browser. - NodeList
+HTMLCollection
+Element
+User
+Objetos do user, são os objetos definidos pelo seu aplicativo. Ou seja, qualquer objeto que você criar ou que importar de alguma biblioteca externa.
+
+*/
